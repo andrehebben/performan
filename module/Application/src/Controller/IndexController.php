b20d6fc7772f457e23a9d9567831ce34328ca89e
Andre Hebben <contact@andrehebben.nl>
2019-10-10 15:25:19 +0100

    added strict type and declared class as final

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
@@ -10,7 +10,7 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
+final class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
