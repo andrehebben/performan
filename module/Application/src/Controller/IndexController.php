778c9ddaf07f1015d6f45b98806c77d1fae74099
Andre Hebben <github@andrehebben.nl>
2019-12-06 16:08:59 +0100

    Added missing module for zend

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -24,13 +24,9 @@ final class IndexController extends AbstractActionController
         // Call the base class' onDispatch() first and grab the response
         $response = parent::onDispatch($e);
 
-        // Set alternative layout
-    //   $this->layout()->setTemplate('layout/layout');
-
         // Return the response
         return $response;
     }
-
 
 
     public function indexAction()
