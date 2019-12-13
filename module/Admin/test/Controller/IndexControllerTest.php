73caf6b871a91159399015680e6418f57948ed9e
Andre Hebben <github@andrehebben.nl>
2019-12-13 14:17:09 +0100

    Added for coverage

---
+++ module/Admin/test/Controller/IndexControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace UserTest\Controller;
+
+use Admin\Controller\AdminController;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class IndexControllerTest extends AbstractHttpControllerTestCase
+{
+    public function setUp(): void
+    {
+        // The module configuration should still be applicable for tests.
+        // You can override configuration here with test case specific values,
+        // such as sample view templates, path stacks, module_listener_options,
+        // etc.
+        $configOverrides = [];
+
+        $this->setApplicationConfig(ArrayUtils::merge(
+            include __DIR__ . '/../../../../config/application.config.php',
+            $configOverrides
+        ));
+
+        parent::setUp();
+    }
+
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->dispatch('/admin', 'GET');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('admin');
+        $this->assertControllerName(AdminController::class); // as specified in router's controller name alias
+        $this->assertControllerClass('AdminController');
+        $this->assertMatchedRouteName('admin');
+    }
+}
---
+++ module/User/test/Controller/AuthControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace UserTest\Controller;
+
+use User\Controller\AuthController;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class AuthControllerTest extends AbstractHttpControllerTestCase
+{
+    public function setUp(): void
+    {
+        // The module configuration should still be applicable for tests.
+        // You can override configuration here with test case specific values,
+        // such as sample view templates, path stacks, module_listener_options,
+        // etc.
+        $configOverrides = [];
+
+        $this->setApplicationConfig(ArrayUtils::merge(
+            include __DIR__ . '/../../../../config/application.config.php',
+            $configOverrides
+        ));
+
+        parent::setUp();
+    }
+
+    public function testLoginActionCanBeAccessed()
+    {
+        $this->dispatch('/login', 'GET');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('user');
+        $this->assertControllerName(AuthController::class); // as specified in router's controller name alias
+        $this->assertControllerClass('AuthController');
+        $this->assertMatchedRouteName('login');
+    }
+}
